1da242dddf340eb0a6017d25b3febc1330cdfe5b
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-06 01:22:09 +0100

    ading docker compose

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World there';
+    return 'Hello World there is a new change !!';
   }
 }
